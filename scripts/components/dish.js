99989639657541b4c99f226f2145e04d1a486198
Calvin Kamp <calvin-kamp@active-value.de>
2025-12-09 03:06:51 +0100

    feat(script): added dish component script

---
+++ scripts/components/dish.js
@@ -0,0 +1,47 @@
+import { cartStore } from "../stores/cart";
+
+export const dish = {
+    vars: {
+        queries: {
+            component:                  '*[data-js=dish]',
+            addToCart:                  '*[data-add-to-cart]',
+
+        },
+
+        attributes: {
+            addToCart:                  'data-add-to-cart'
+        }
+    },
+
+    init() {
+
+        const $dishes = document.querySelectorAll(this.vars.queries.component);
+        
+        if(!$dishes) {
+            return;
+        }
+
+        this.addEventTrigger($dishes)
+
+    },
+
+    addEventTrigger($dishComponents) {
+
+        for(const $dish of $dishComponents) {
+
+            const $addToCart = $dish.querySelector(this.vars.queries.addToCart);
+
+            $addToCart.addEventListener('click', () => {
+
+                const dishId = $addToCart.getAttribute(this.vars.attributes.addToCart);
+                
+                cartStore.addToCart(dishId);
+                
+
+            })
+
+        }
+
+    }
+
+}
