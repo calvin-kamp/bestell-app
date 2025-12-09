fccdcf38507f4012d4478c89f0ed0a9689bdb09b
Calvin Kamp <calvin.kamp6696@gmail.com>
2025-12-09 16:28:27 +0100

    feat(script): - added cart-layout-helper for dynamic cart rendering

---
+++ scripts/utils/cart-layout-helper.js
@@ -0,0 +1,66 @@
+import { renderCart } from '../page.js';
+import { cartStore } from '../stores/cart.js';
+
+export const cartLayoutHelper  = {
+
+    vars: {
+        windowWidth:        0,
+        bigBreakpoint:      null,
+        cartStore:          {}
+    },
+
+    init() {
+
+        this.setVariables();
+        this.addEventTrigger();
+
+        if(this.vars.bigBreakpoint) {
+            renderCart(this.vars.cartStore, 'aside');
+        } else {
+            renderCart(this.vars.cartStore, 'dialog');
+        }
+
+    },
+
+    setVariables() {
+
+        this.vars.windowWidth = window.innerWidth;
+        this.vars.cartStore = cartStore.getCartData();
+        this.vars.bigBreakpoint = this.vars.windowWidth >= 1024;
+
+    },
+
+    addEventTrigger() {
+
+        window.addEventListener('resize', () => {
+
+            this.handleResize();
+
+        })
+
+    },
+
+    handleResize() {
+
+        this.vars.windowWidth = window.innerWidth;
+        const isBigBreakpoint = this.vars.windowWidth >= 1024;
+
+        if(isBigBreakpoint === this.vars.bigBreakpoint) {
+            return;
+        }
+
+        this.vars.bigBreakpoint = isBigBreakpoint;
+
+        if(isBigBreakpoint) {
+
+            renderCart(this.vars.cartStore, 'aside');
+
+        } else {
+
+            renderCart(this.vars.cartStore, 'dialog');
+
+        }
+
+    }
+
+}
